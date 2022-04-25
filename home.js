af49aef24434bc33c6923970c788674f4fa072ef
Domyygamer <91354992+Domyygamer@users.noreply.github.com>
2022-04-25 17:56:31 +0200

    ''

---
+++ home.js
@@ -0,0 +1,37 @@
+import { debounce, lazyLoading } from "./utils.js";
+
+let header = document.querySelector("header");
+window.addEventListener("scroll", debounce(navFixPos));
+function navFixPos(e) {
+  if (window.scrollY == 0) {
+    header.classList.remove("fixed");
+    return;
+  }
+
+  header.classList.add("fixed");
+}
+
+let faqQuestions = document.querySelectorAll("[data-question]");
+faqQuestions.forEach((q) => {
+  q.addEventListener("click", toggleAnswer);
+
+  function toggleAnswer(e) {
+    let icon = q.querySelector("ion-icon");
+    let answer = q.querySelector("p.answer");
+    icon.name.includes("up")
+      ? (icon.name = "chevron-down-outline")
+      : (icon.name = "chevron-up-outline");
+
+    answer.classList.toggle("active");
+  }
+});
+
+//* animating lazy  loading with Interaction Observer;
+function scrollingAnimations() {
+  let allElements = Array.from(
+    document.querySelectorAll(":is(section, footer) .container > *")
+  );
+
+  lazyLoading(allElements);
+}
+scrollingAnimations();
---
+++ utils.js
@@ -0,0 +1,30 @@
+function debounce(func, wait = 100, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    }, wait);
+    if (immediate && !timeout) func.apply(context, args);
+  };
+}
+
+function lazyLoading(elements) {
+  let observer = new IntersectionObserver(
+    (entries) =>
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fadeUp");
+          observer.unobserve(entry.target);
+        }
+      }),
+    { rootMargin: "30%" }
+  );
+
+  elements.forEach((e) => observer.observe(e));
+}
+
+export { debounce, lazyLoading };
